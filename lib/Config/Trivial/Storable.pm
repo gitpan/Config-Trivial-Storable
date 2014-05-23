5e3d1b8b2eb94c8ab38c3c0db2abcd16f31d52f3
Adam John Trickett <atrickett@cpan.org>
2014-05-23 13:05:03 +0000

    Import of ATRICKETT/Config-Trivial-Storable-0.31 from CPAN.
    gitpan-cpan-distribution: Config-Trivial-Storable
    gitpan-cpan-version:      0.31
    gitpan-cpan-path:         ATRICKETT/Config-Trivial-Storable-0.31.tar.gz
    gitpan-cpan-author:       ATRICKETT
    gitpan-cpan-maturity:     released

--- lib/Config/Trivial/Storable.pm
+++ lib/Config/Trivial/Storable.pm
@@ -1,4 +1,4 @@
-#   $Id: Storable.pm 65 2014-05-21 22:35:09Z adam $
+#   $Id: Storable.pm 69 2014-05-23 10:27:30Z adam $
 
 package Config::Trivial::Storable;
 
@@ -10,7 +10,7 @@ use Carp;
 use warnings;
 use Storable qw(lock_store lock_retrieve);
 
-our $VERSION = '0.31_3';
+our $VERSION = '0.31';
 my ( $_package, $_file ) = caller;
 
 #
@@ -265,23 +265,22 @@ L<perl>, L<Config::Trivial>, L<Storable>.
 
 =head1 LICENSE AND COPYRIGHT
 
-This version as C<Config::Trivial::Storable>, Copyright iredale consulting 2006-2007
+This version as C<Config::Trivial::Storable>, Copyright Adam John Trickett 2006-2014
 
 OSI Certified Open Source Software.
+Free Software Foundation Free Software.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published
-by the Free Software Foundation; either version 2 of the License,
-or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-MA  02111, USA.
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
--- t/01-basic.t
+++ t/01-basic.t
@@ -1,11 +1,11 @@
-#   $Id: 01-basic.t 64 2014-05-21 22:34:42Z adam $
+#   $Id: 01-basic.t 69 2014-05-23 10:27:30Z adam $
 
 use strict;
 use Test::More tests => 4;
 
 BEGIN { use_ok( 'Config::Trivial::Storable' ); }
 
-is( $Config::Trivial::Storable::VERSION, "0.31_3", 'Version Check' );
+is( $Config::Trivial::Storable::VERSION, "0.31", 'Version Check' );
 
 my $config = Config::Trivial::Storable->new;
 ok(defined $config, 'Object is defined' );
